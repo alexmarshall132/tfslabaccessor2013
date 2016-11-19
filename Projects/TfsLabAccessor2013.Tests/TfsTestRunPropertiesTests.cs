7b20dab3694c28c5f350cbf39a06448e12ca9a3d
Alex Marshall <AJMarshall@pcl.com>
2016-11-19 13:16:41 -0700

    Initial implementation of a tool for providing access to TFS properties and TFS itself within Lab-managed MSTest Test Runs.

---
+++ Projects/TfsLabAccessor2013.Tests/TfsTestRunPropertiesTests.cs
@@ -0,0 +1,67 @@
+﻿//  -----------------------------------------------------------------------
+//  <copyright file="TfsTestRunPropertiesTests.cs" company="West Peaks Consulting.">
+//      Copyright (c) West Peaks Consulting. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace TfsLabAccessor2013.Tests
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Test suite for the <see cref="TfsTestRunProperties"/> class.
+    /// </summary>
+    [TestClass]
+    public class TfsTestRunPropertiesTests
+    {
+        /// <summary>
+        /// Verifies that all properties are correctly parsed
+        /// </summary>
+        [TestMethod, TestCategory("StandAlone")]
+        [Description("Verifies that all properties are correctly parsed")]
+        public void CreateCorrectlyParsesValidProperties()
+        {
+            Dictionary<string, string> testProperties = new Dictionary<string, string>
+            {
+                { "TestName", "CodedUITestMethod1" },
+                { "__Tfs_BuildUri__", "vstfs:///Build/Build/18" },
+                { "__Tfs_IsInLabEnvironment__", "True" },
+                { "__Tfs_TestRunId__", "22" },
+                { "__Tfs_TestCaseId__", "117" },
+                { "__Tfs_TeamProject__", "MyTeamProjectName" },
+                { "__Tfs_BuildDirectory__", @"\\vsalm\ffdrops\New Build Definition 1\New Build Definition 1_20130222.7" },
+                { "__Tfs_LabEnvironment__", @"<?xml version=""1.0"" encoding=""utf-16""?><LabEnvironment Id=""5f37b167-ad24-4f7e-bb1e-2e65a3e71a1f"" Name=""Windows 7 Client"" Uri=""vstfs:///LabManagement/LabEnvironment/2""><LabSystems><LabSystem Name=""TestClient"" ComputerName=""TestClient"" Roles=""Desktop Client""><CustomProperties /></LabSystem></LabSystems></LabEnvironment>" },
+                { "__Tfs_TestConfigurationId__", @"2" },
+                { "__Tfs_TestPlanId__", @"4" },
+                { "__Tfs_TestConfigurationName__", @"Chrome" },
+                { "__Tfs_TestPointId__", @"12" },
+                { "__Tfs_TfsServerCollectionUrl__", @"http://vsalm:8080/tfs/fabrikamfibercollection" },
+                { "__Tfs_BuildPlatform__", @"Any CPU" },
+                { "__Tfs_BuildNumber__", @"New Build Definition 1_20130222.7" },
+                { "__Tfs_BuildFlavor__", @"Debug" },
+                { "__Tfs_BuildConfigurationId__", @"22" }
+            };
+
+            TfsTestRunProperties runProperties = TfsTestRunProperties.Create(testProperties);
+
+            Assert.IsNotNull(runProperties, "The run properties returned from Create must never be null");
+            Assert.AreEqual("vstfs:///Build/Build/18", runProperties.BuildUri.AbsoluteUri, "The build URIs must match");
+            Assert.IsTrue(runProperties.IsInLabEnvironment, "Should have been in a lab environment");
+            Assert.AreEqual(22, runProperties.TestRunId, "TestRunId must match");
+            Assert.AreEqual(117, runProperties.TestCaseId, "TestCaseId must match");
+            Assert.AreEqual("MyTeamProjectName", runProperties.TeamProject, "TeamProject must match");
+            Assert.AreEqual(@"\\vsalm\ffdrops\New Build Definition 1\New Build Definition 1_20130222.7", runProperties.BuildDirectory, "BuildDirectory must match");
+            Assert.IsNotNull(runProperties.LabEnvironment, "The lab environment must not be null");
+            Assert.AreEqual(2, runProperties.TestConfigurationId, "TestConfigurationId must match");
+            Assert.AreEqual(4, runProperties.TestPlanId, "TestPlanId must match");
+            Assert.AreEqual("Chrome", runProperties.TestConfigurationName, "TestConfigurationName must match");
+            Assert.AreEqual(12, runProperties.TestPointId, "TestPointId must match");
+            Assert.AreEqual(@"http://vsalm:8080/tfs/fabrikamfibercollection", runProperties.TfsServerCollectionUrl.AbsoluteUri, "TfsServerCollectionUrl must match");
+            Assert.AreEqual("Any CPU", runProperties.BuildPlatform, "BuildPlatform must match");
+            Assert.AreEqual("New Build Definition 1_20130222.7", runProperties.BuildNumber, "BuildNumber must match");
+            Assert.AreEqual("Debug", runProperties.BuildFlavor, "BuildFlavor must match");
+            Assert.AreEqual(22, runProperties.BuildConfigurationId, "BuildConfigurationId must match");
+        }
+    }
+}
---
+++ Projects/TfsLabAccessor2013/TfsTestRunProperties.cs
@@ -0,0 +1,182 @@
+﻿
+namespace TfsLabAccessor2013
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Reflection;
+    using System.Xml.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Represents the properties for a Test Run injected into tests during a TFS Lab Managed
+    /// test run execution.
+    /// </summary>
+    public class TfsTestRunProperties
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TfsTestRunProperties"/>
+        /// </summary>
+        private TfsTestRunProperties()
+        {
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Uri"/> of the build in TFS.
+        /// </summary>
+        public Uri BuildUri { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the build is currently running in a Lab managed environment.
+        /// </summary>
+        public bool IsInLabEnvironment { get; private set; }
+
+        /// <summary>
+        /// Gets the ID of the current test run within TFS.
+        /// </summary>
+        public int TestRunId { get; private set; }
+
+        /// <summary>
+        /// Gets the ID of the Test Case associated with the currently executing test method.
+        /// </summary>
+        public int TestCaseId { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the Team Project in which the currently executing Test Plan is defined.
+        /// </summary>
+        public string TeamProject { get; private set; }
+
+        /// <summary>
+        /// Gets the current build directory.
+        /// </summary>
+        public string BuildDirectory { get; private set; }
+
+        /// <summary>
+        /// Gets the XML representation of the lab environment executing the Test Plan.
+        /// </summary>
+        public XDocument LabEnvironment { get; private set; }
+
+        /// <summary>
+        /// Gets the ID of the configuration used with the Test Plan.
+        /// </summary>
+        public int TestConfigurationId { get; private set; }
+
+        /// <summary>
+        /// Gets the ID of the currently executing Test Plan.
+        /// </summary>
+        public int TestPlanId { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the test configuration used with the currently executing Test Plan.
+        /// </summary>
+        public string TestConfigurationName { get; private set; }
+
+        /// <summary>
+        /// Gets the ID of the Test Point associated with the currently executing Test Method.
+        /// </summary>
+        public int TestPointId { get; private set; }
+
+        /// <summary>
+        /// Gets the URL of the TFS Team Project Collection that holds the Team Project that owns
+        /// the currently executing Test Method.
+        /// </summary>
+        public Uri TfsServerCollectionUrl { get; private set; }
+
+        /// <summary>
+        /// Gets the build platform of the current test run.
+        /// </summary>
+        public string BuildPlatform { get; private set; }
+
+        /// <summary>
+        /// Gets the build configuration (aka build flavor) of the test build that's currently executing
+        /// </summary>
+        public string BuildFlavor { get; private set; }
+
+        /// <summary>
+        /// Gets the build number that's currently executing.
+        /// </summary>
+        public string BuildNumber { get; private set; }
+
+        /// <summary>
+        /// Gets the ID of the build configuration.
+        /// </summary>
+        public int BuildConfigurationId { get; private set; }
+
+        /// <summary>
+        /// Gets a boolean value indicating whether or not this instance is valid, i.e. properties
+        /// were found to populate it.
+        /// </summary>
+        public bool IsValid { get; private set; }
+
+        /// <summary>
+        /// Creates a new <see cref="TfsTestRunProperties"/> instance from the given <paramref name="testProperties"/>
+        /// </summary>
+        /// <param name="testProperties">
+        /// The test properties from which we want to extract the TFS Test Run settings. Must not be null.
+        /// This should typically be <see cref="TestContext.Properties"/>
+        /// </param>
+        /// <returns>
+        /// A new <see cref="TfsTestRunProperties"/> instance generated from the given <paramref name="testProperties"/>.
+        /// Guaranteed not to be null.
+        /// </returns>
+        public static TfsTestRunProperties Create(IDictionary testProperties)
+        {
+            if (testProperties == null)
+            {
+                throw new ArgumentNullException("testProperties");
+            }
+
+            Dictionary<string, object> properties = testProperties.Keys.OfType<string>()
+                .Where(k => k.StartsWith("__Tfs_") && k.EndsWith("__"))
+                .ToDictionary(x => x.TrimStart('_').TrimEnd('_').Substring("Tfs_".Length), k => testProperties[k]);
+
+            TfsTestRunProperties runProperties = new TfsTestRunProperties();
+
+            if (properties.Count == 0)
+            {
+                return runProperties;
+            }
+
+            foreach (var kvp in properties)
+            {
+                PropertyInfo property = runProperties.GetType().GetProperty(kvp.Key);
+
+                if (property == null)
+                {
+                    continue;
+                }
+
+                if (kvp.Value == null || (String.Empty.Equals(kvp.Value) && property.PropertyType != typeof(string)))
+                {
+                    continue;
+                }
+
+                // Special case because there's no default converter for XDocument.
+                if (property.Name == "LabEnvironment")
+                {
+                    runProperties.LabEnvironment = XDocument.Parse(Convert.ToString(kvp.Value));
+
+                    continue;
+                }
+
+                TypeConverter converter = TypeDescriptor.GetConverter(property.PropertyType);
+
+                if (converter == null)
+                {
+                    Trace.TraceError("Failed to find converter for type '{0}'", property.PropertyType.FullName);
+
+                    continue;
+                }
+
+                property.GetSetMethod(true).Invoke(runProperties, new[] { converter.ConvertFrom(kvp.Value) });
+            }
+
+            runProperties.IsValid = true;
+
+            return runProperties;
+        }
+    }
+}
